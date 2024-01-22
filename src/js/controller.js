7b13a054f9e63c932489e5bf6cec3135df462601
bokkle <mitch.r_@hotmail.com>
2024-01-22 13:22:52 -0500

    implemented search results and pagination

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,27 +1,19 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
+import resultsView from './views/resultView.js';
+import paginationView from './views/paginationView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async () => {
   try {
     const id = window.location.hash.slice(1);
-    console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
@@ -31,12 +23,41 @@ const controlRecipes = async () => {
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
-['hashchange', 'load'].forEach((ev) =>
-  window.addEventListener(ev, controlRecipes)
-);
+const controlSearchResults = async () => {
+  try {
+    resultsView.renderSpinner();
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.getSearchResultsPage());
+
+    // 4) Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const controlPagination = (goToPage) => {
+  // 3) Render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 4) Render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
+
+const init = () => {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,42 +1,27 @@
+import View from './View.js';
+
 import icons from 'url:../../img/icons.svg';
 import fracty from 'fracty';
-console.log(fracty);
 
-class RecipeView {
-  #parentElement = document.querySelector('.recipe');
-  #data;
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _errorMessage = 'We could not find that recipe. Please try another.';
+  _message = '';
 
-  render(data) {
-    this.#data = data;
-    const markup = this.#generateMarkup();
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  addHandlerRender(handler) {
+    ['hashchange', 'load'].forEach((ev) =>
+      window.addEventListener(ev, handler)
+    );
   }
 
-  #clear() {
-    this.#parentElement.innerHTML = '';
-  }
-
-  renderSpinner = () => {
-    const markup = `
-      <div class="spinner">
-          <svg>
-              <use href="${icons}.svg#icon-loader"></use>
-          </svg>
-      </div>
-      `;
-    this.#parentElement.innerHTML = '';
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
-
-  #generateMarkup() {
+  _generateMarkup() {
     return `
     <figure class="recipe__fig">
-        <img src="${this.#data.image}" alt="${
-      this.#data.title
+        <img src="${this._data.image}" alt="${
+      this._data.title
     }" class="recipe__img" />
         <h1 class="recipe__title">
-        <span>${this.#data.title}</span>
+        <span>${this._data.title}</span>
         </h1>
     </figure>
 
@@ -46,7 +31,7 @@ class RecipeView {
             <use href="${icons}.svg#icon-clock"></use>
         </svg>
         <span class="recipe__info-data recipe__info-data--minutes">${
-          this.#data.cookingTime
+          this._data.cookingTime
         }</span>
         <span class="recipe__info-text">minutes</span>
         </div>
@@ -55,7 +40,7 @@ class RecipeView {
             <use href="${icons}.svg#icon-users"></use>
         </svg>
         <span class="recipe__info-data recipe__info-data--people">${
-          this.#data.servings
+          this._data.servings
         }</span>
         <span class="recipe__info-text">servings</span>
 
@@ -74,9 +59,6 @@ class RecipeView {
         </div>
 
         <div class="recipe__user-generated">
-        <svg>
-            <use href="${icons}.svg#icon-user"></use>
-        </svg>
         </div>
         <button class="btn--round">
         <svg class="">
@@ -88,8 +70,8 @@ class RecipeView {
     <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-            ${this.#data.ingredients
-              .map(this.#generateMarkupIngredient)
+            ${this._data.ingredients
+              .map(this._generateMarkupIngredient)
               .join('')}
         </ul>
     </div>
@@ -99,13 +81,13 @@ class RecipeView {
         <p class="recipe__directions-text">
         This recipe was carefully designed and tested by
         <span class="recipe__publisher">${
-          this.#data.publisher
+          this._data.publisher
         }</span>. Please check out
         directions at their website.
         </p>
         <a
         class="btn--small recipe__btn"
-        href="${this.#data.sourceUrl}"
+        href="${this._data.sourceUrl}"
         target="_blank"
         >
         <span>Directions</span>
@@ -117,7 +99,7 @@ class RecipeView {
 `;
   }
 
-  #generateMarkupIngredient(ing) {
+  _generateMarkupIngredient(ing) {
     return `
         <li class="recipe__ingredient">
             <svg class="recipe__icon">
